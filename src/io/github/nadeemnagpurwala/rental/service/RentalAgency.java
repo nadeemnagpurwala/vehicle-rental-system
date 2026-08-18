e16f3b7b4de6d3cf8483d018d3b5b4cefac19b83
Nadeem Nagpurwala <nagpurwalanadeem@gmail.com>
2026-08-18 22:36:16 +0530

    Refactor: Replace fixed-size array with ArrayList for fleet storage

--- src/io/github/nadeemnagpurwala/rental/service/RentalAgency.java
+++ src/io/github/nadeemnagpurwala/rental/service/RentalAgency.java
@@ -1,33 +1,24 @@
 package io.github.nadeemnagpurwala.rental.service;
 
-import io.github.nadeemnagpurwala.rental.exception.VehicleNotAvailableException;
-import io.github.nadeemnagpurwala.rental.exception.VehicleNotFoundException;
-import io.github.nadeemnagpurwala.rental.model.Insurable;
-import io.github.nadeemnagpurwala.rental.model.RentalReceipt;
-import io.github.nadeemnagpurwala.rental.model.Vehicle;
-import io.github.nadeemnagpurwala.rental.model.VehicleAvailability;
+import io.github.nadeemnagpurwala.rental.exception.*;
+import io.github.nadeemnagpurwala.rental.model.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class RentalAgency {
-    private static final int MAX_FLEET_SIZE = 5;
-    private Vehicle[] fleet = new Vehicle[MAX_FLEET_SIZE];
-    private int count = 0;
+    private List<Vehicle> fleet = new ArrayList<>();
 
     public void addVehicle(Vehicle vehicle){
-        if (count >= MAX_FLEET_SIZE) {
-            System.out.println("Maximum vehicles added, the fleet is currently full");
-        } else {
-            fleet[count] = vehicle;
-            count++;
-        }
+        fleet.add(vehicle);
     }
 
     public void displayVehicles() {
-        if (count > 0) {
+        if (!fleet.isEmpty()) {
             System.out.println("We have following list of vehicles available in our fleet :");
             System.out.println();
-            for (int i = 0; i < count; i++) {
-                fleet[i].printDetails();
-                if (fleet[i] instanceof Insurable insurable) {
+            for (Vehicle vehicleFleet : fleet) {
+                vehicleFleet.printDetails();
+                if (vehicleFleet instanceof Insurable insurable) {
                     System.out.printf("Daily insurance cost in INR : %.2f INR%n", insurable.calculateInsurance());
                 }
                 System.out.println("*".repeat(20));
@@ -62,9 +53,9 @@ public class RentalAgency {
     }
 
     private Vehicle findVehicleById(int id) {
-        for (int i = 0; i < count; i++) {
-            if (fleet[i].getId() == id) {
-                return fleet[i];
+        for (Vehicle vehicleFleet : fleet) {
+            if (vehicleFleet.getId() == id) {
+                return vehicleFleet;
             }
         }
         return null;
